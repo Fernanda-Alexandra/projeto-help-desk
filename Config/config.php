7a269cd944188f4be726ca58f2101feb72f7908f
Fernanda Alexandra <fernandinha_camarao@hotmail.com>
2019-06-16 17:59:46 -0300

    alterando o nome do banco de dados

--- Config/config.php
+++ Config/config.php
@@ -3,6 +3,6 @@
     define('DB_HOSTNAME', 'localhost');
 	define('DB_USERNAME', 'root');
 	define('DB_PASSWORD', 'root');
-	define('DB_DATABASE', 'baseconhecimento');
+	define('DB_DATABASE', 'baseconhecimento0505');
 	define('DB_CHARSET', 'utf8');
 	
