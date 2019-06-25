67d336b9a965447f643d2bc5bc557c9cd47b2755
Luan <luanlima2010@bol.com.br>
2019-06-25 18:11:32 -0300

    handle-actions-generic-controller
    Atualizando generic controller para recuperar os actions da URL e redirecionar para o método adequado.

--- Controller/GenericController.php
+++ Controller/GenericController.php
@@ -2,6 +2,68 @@
 abstract class GenericController{
 
   protected $repository;
+	
+	 function __construct() 
+    {
+     
+        $this->getParams();
+  
+    } 
+    public function getParams()
+    {
+        $acao = isset($_GET['action']) ? $_GET['action'] : null;
+        if($acao)
+        {
+         switch ($acao) {
+
+                case "NOVO":
+               // $this->abrirFormulario();
+                break;
+                case "ACESSAR":
+                $id = isset($_GET['id']) ? $_GET['id'] : null;
+               // $this->get($id);
+                break;
+              case "DETALHAR":
+                $id = isset($_GET['id']) ? $_GET['id'] : null;
+               // $this->detalhar($id);
+                break;
+                case "ATUALIZAR":
+                $id = isset($_GET['id']) ? $_GET['id'] : null;
+                //$this->atualizar($id);
+                break;
+                case "SALVAR":
+               // $this->salvar();
+                break;
+                case "EXCLUIR":
+                $id = isset($_GET['id']) ? $_GET['id'] : null;
+                //$this->excluir($id);
+                break;
+                case "LISTAR":
+                $id = isset($_GET['id']) ? $_GET['id'] : null;
+               // $this->listar($id);
+                break;
+                
+                default:
+                echo "<b>[WARNING] A ação solicitada não é reconhecida</b>";
+                        }
+
+    }  
+
+     $this->limpaGets();  
+
+    }
+   protected function limpaGets()
+    {
+        if(isset($_GET['acao']))
+        {
+            unset($_GET['acao']);
+        }
+
+        if(isset($_GET['id']))
+       {
+        unset($_GET['id']);
+       } 
+    }
 
 
      protected function index()
